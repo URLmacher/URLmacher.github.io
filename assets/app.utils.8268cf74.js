c0fb949dc852f1ddefcda73b00b1183970fb0d50
Patrick <patrick@chax.at>
2024-11-01 21:20:04 +0100

    new quotes and pictures

---
+++ assets/app.utils.8268cf74.js
@@ -0,0 +1,1 @@
+import{i as o}from"./index.0a52fb5a.js";const{t:s}=o.global,i=(t,e={})=>{document.title=s(t,e)};export{i as s};
---
+++ assets/i18n.ba06c197.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index.42b87c24.js";import{i}from"./index.0a52fb5a.js";import"./vue-i18n.763ad005.js";var p=r(({app:o})=>{o.use(i)});export{p as default};
