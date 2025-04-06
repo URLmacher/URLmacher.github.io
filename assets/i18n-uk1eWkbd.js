6018399a850caf3c3efb63e85862448f03eaaf45
URLmacher <urlmacher@gmail.com>
2025-04-06 16:13:49 +0200

    fixed interpolation

--- assets/i18n-uk1eWkbd.js
+++ assets/i18n-uk1eWkbd.js
@@ -1,4 +1,4 @@
-import{d as De,h as st,i as rn,o as an,a as ln,c as le,w as be,F as ot,g as ct,e as sn,b as on,r as cn,s as un,f as _n,T as mn,j as fn}from"./index-ChwAL5_a.js";/*!
+import{d as De,h as st,i as rn,o as an,a as ln,c as le,w as be,F as ot,g as ct,e as sn,b as on,r as cn,s as un,f as _n,T as mn,j as fn}from"./index-yfJftCh9.js";/*!
   * shared v9.14.2
   * (c) 2024 kazuya kawaguchi
   * Released under the MIT License.
